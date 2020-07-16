02751fa09bdd9e40d537439c74da549e6bfe471e
Cao Zheng <zzhhdsf@gmail.com>
2020-07-16 18:05:35 -0400

    array

--- src/Q0219/Q0219.java
+++ src/Q0219/Q0219.java
@@ -19,21 +19,28 @@ import java.util.Set;
 //Output: false
 public class Q0219 {
     public static void main(String[] args) {
-        int [] nums ={1,2,3,1,2,3};
-        int k=2;
-        System.out.println(containsNearbyDuplicate(nums,k));
+        int[] nums = {1, 2, 3, 1, 2, 3};
+        int k = 2;
+        System.out.println(containsNearbyDuplicate(nums, k));
     }
 
     public static boolean containsNearbyDuplicate(int[] nums, int k) {
         Set<Integer> set = new HashSet<Integer>();
-        for(int i=0;i<nums.length;i++){
-            if(set.contains(nums[i])){
-                for(int j=0;j<i;j++){
-                    if(nums[j]==nums[i]&&i-j<=k) return true;
-                }
-            }
-            set.add(nums[i]);
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k) set.remove(nums[i - k - 1]);
+            if (set.add(nums[i]) == false) return true;
         }
         return false;
+
+//        Set<Integer> set = new HashSet<Integer>();
+//        for(int i=0;i<nums.length;i++){
+//            if(set.contains(nums[i])){
+//                for(int j=0;j<i;j++){
+//                    if(nums[j]==nums[i]&&i-j<=k) return true;
+//                }
+//            }
+//            set.add(nums[i]);
+//        }
+//        return false;
     }
 }
