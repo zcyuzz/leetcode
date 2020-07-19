75126a9aaa886d4fba64d27841cf5125a5fe1724
Cao Zheng <zzhhdsf@gmail.com>
2020-07-18 22:07:41 -0400

    array and hash table

--- src/Q0220/Q0220.java
+++ src/Q0220/Q0220.java
@@ -1,8 +1,12 @@
 package Q0220;
 
 import java.awt.*;
+import java.util.HashSet;
+import java.util.Set;
 
-//Given an array of integers, find out whether there are two distinct indices i and j in the array such that the absolute difference between nums[i] and nums[j] is at most t and the absolute difference between i and j is at most k.
+//Given an array of integers, find out whether there are two distinct indices i and j in the array such that
+// the absolute difference between nums[i] and nums[j] is at most t and
+// the absolute difference between i and j is at most k.
 //
 //Example 1:
 //
@@ -18,13 +22,24 @@ import java.awt.*;
 //Output: false
 public class Q0220 {
     public static void main(String[] args) {
-        int nums [] = {1,5,9,1,5,9};
-        int k =2;
-        int t =3;
+        int nums [] = {2147483647,-2147483647};
+        int k =1;
+        int t =2147483647;
         System.out.println(containsNearbyAlmostDuplicate(nums,k,t));
 
     }
     public static boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        long absDiff;
+        for(int i =0;i<nums.length;i++){
+            for(int j=i+1;j<i+k+1&&j<nums.length;j++){
+                System.out.println(i + " " +j);
+                System.out.println("t "+Math.abs(nums[j]-nums[i]));
+                System.out.println("k " + Math.abs(j-i));
+                absDiff= Math.abs(((long)nums[j]-(long)nums[i]));
+                if(absDiff<=t) return true;
+            }
+        }
+        return false;
 
     }
 }
